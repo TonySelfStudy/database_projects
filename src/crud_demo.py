d717194d7c406daaafe295974e1f6106f1ecf46d
Tony Held <tony.held@gmail.com>
2021-03-03 10:10:20 -0800

    1st commit for new github location

--- src/crud_demo.py
+++ src/crud_demo.py
@@ -1,15 +1,23 @@
+# Resume at 4. Working with Image and Blob Data
+
 import sqlite3
+import datetime
+from prettytable import PrettyTable
+import pandas as pd
 
 
 def open_db(file_name):
+    print(f'Connecting to dbase: {file_name}')
     conn = sqlite3.connect(file_name)
-    print(f'Connected to dbase: {file_name}')
-    print(conn)
+    print(f'Connected')
+
+    print(f'{conn=}')
     return conn
 
 
 def create_product_table(conn):
-    conn.cursor()
+
+    cursor = conn.cursor()
 
     txt = '''
     create table if not exists product(
@@ -20,13 +28,98 @@ def create_product_table(conn):
     created datetime)
     '''
 
-    conn.execute(txt)
+    cursor.execute(txt)
     conn.commit()
-    print('Created a table')
+    cursor.close()
+
+    print('Created/Verified the product table')
+
+
+def create_data(conn):
+
+    print('Creating Data')
+    cursor = conn.cursor()
+
+    insert_sql = ('insert into product (name, stock, price, created) '
+                        'values (?, ?, ?, ?)')
+
+    for i in range(1, 6):
+        params = ("product" + str(i),
+                  (100 + i),
+                  (3.14 * i),
+                  datetime.datetime.now())
+        cursor.execute(insert_sql, params)
+        product_id = cursor.lastrowid
+        print(f'Created product_id: {product_id}')
+
+    conn.commit()
+    cursor.close()
+    print('Data added to product table')
+
+def read_data(conn):
+    print('Reading Data')
+
+    cursor = conn.cursor()
+    sql = 'select * from product'
+    cursor.execute(sql)
+
+    # print('Bulk Read')
+    # for i, row in enumerate(cursor):
+    #     print(f'Row {i}: {row}')
+
+    print('Specified Read and Formatted output')
+    t = PrettyTable(['ID', 'Name', 'Stock', 'Price', 'Created'])
+    cursor.execute(sql)
+    for (id, name, stock, price, created) in cursor:
+        t.add_row([id, name, stock, format(price, '.2f'), created])
+
+    print(t)
+
+    print('Data read to product table')
+
+
+def update_data(conn, id, name, stock, price):
+    print('Updating Data')
+    cursor = conn.cursor()
+
+    sql = ('update product set name=?, stock=?, price=? '
+           'where id = ?')
+    params = (name, stock, price, id)
+    cursor.execute(sql, params)
+
+    conn.commit()
+    cursor.close
+    print('Done updating')
+
+def del_data(id):
+    print(f'Deleting Data for {id=}')
+    cursor = conn.cursor()
+
+    sql = ('delete from product where id = ?')
+    params = (id,)
+    cursor.execute(sql, params)
+
+    print(f'{cursor.rowcount} row(s) deleted from product table')
+
+    conn.commit()
+    cursor.close
+    print('Done updating')
 
 
 if __name__ == '__main__':
     db_file_name = 'pydb.db'
     conn = open_db(db_file_name)
     create_product_table(conn)
+    # create_data(conn)
+    read_data(conn)
+
+    id_ = 15
+    update_data(conn, id_, 'new prod id', id_+100, id_*0.1)
+    del_data(18)
+    read_data(conn)
+
+    panda_party = pd.read_sql_query('select * from product', conn)
+    print(panda_party)
+
+
     conn.close()
---
+++ src/db_image_data.py
@@ -0,0 +1,73 @@
+import sqlite3
+import datetime
+from prettytable import PrettyTable
+
+def open_db(file_name):
+    print(f'Connecting to dbase: {file_name}')
+    conn = sqlite3.connect(file_name)
+    print(f'Connected')
+
+    print(f'{conn=}')
+    return conn
+
+def create_table(conn):
+    cursor = conn.cursor()
+
+    sql = ('create table if not exists imagefiles('
+           'id integer primary key autoincrement,'
+           'filename char(30) not null,'
+           'filetype char(15) not null,'
+           'imgfile blob,'
+           'created datetime)')
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    print('Created table if needed')
+
+def insert_image(conn, fn):
+    with open(fn, 'rb') as fid:
+        image = fid.read()
+
+    sql = ('insert into imagefiles (filename, filetype, imgfile, created)'
+           'values (?,?,?,?)')
+    params = (fn, fn.split('.')[1], image, datetime.datetime.now())
+
+
+    cursor = conn.cursor()
+    cursor.execute(sql, params)
+    img_id = cursor.lastrowid
+    print(f'Inserted file: {fn} as id: {img_id}')
+
+    conn.commit()
+    cursor.close()
+
+def extract_image(conn, id):
+    sql = ('select filename, imgfile from imagefiles where id=?')
+    params = (id,)
+
+    cursor = conn.cursor()
+
+    cursor.execute(sql, params)
+
+    for (filename, imgfile) in cursor:
+        print(f"Saving filename: {'copy' + filename}")
+
+        with open('copy' + filename, 'wb') as fid:
+            fid.write(imgfile)
+
+
+if __name__  == '__main__':
+    db_name = 'image_blob.db'
+    conn1 = open_db(db_name)
+
+    create_table(conn1)
+
+    fn_image = 'image1.png'
+    insert_image(conn1, fn_image)
+
+    print('Extracting file from database to new file')
+    extract_image(conn1, 5)
+
+    print('Closing connection')
+    conn1.close()
+    print('Done!')
